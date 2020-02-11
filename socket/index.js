1b68e99faad47e26c3770ac963d6c091d1efae52
JGil <jgil@papamono.es>
2020-02-11 17:04:03 +0100

    fin sockets

--- socket/index.js
+++ socket/index.js
@@ -1,12 +1,20 @@
-module.exports=function(io){
-    io.on('connection', function(socket){
-        console.log('connection has been established');
-        
-        //Esto viene del cliente
-        socket.on('meetup/postSave', function(post){
-            //Para el cliente
-            io.emit('meetup/postPublished', post)
-        })
+module.exports = function (io) {
+    io.on('connection', function(socket) {
+      console.log('connection has been established');
+  
+      socket.on('meetup/subscribe', function(meetupId) {
+        console.log('joining meetup ', `meetup-${meetupId}`);
+        socket.join(`meetup-${meetupId}`);
+      })
+  
+      socket.on('meetup/unsubscribe', function(meetupId) {
+        console.log('leaving meetup ', `meetup-${meetupId}`);
+        socket.leave(`meetup-${meetupId}`);
+      })
+  
+      socket.on('meetup/postSaved', function(post) {
+        console.log('emitting to meetup', `meetup-${post.meetup}`)
+        socket.to(`meetup-${post.meetup}`).emit('meetup/postPublished', post)
+      })
     })
-}
-
+  }
